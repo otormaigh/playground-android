0553bdbd03426e62fd049d90d421a240facd6882
Elliot Tormey <elliot.tormey@gmail.com>
2021-03-13 21:39:42 +0000

    add Photo.sq table, without migration update (this should fail a CI build)

---
+++ library/persistence/src/main/java/ie/otormaigh/playground/persistence/columnadapter/RoverColumnAdapter.kt
@@ -0,0 +1,14 @@
+package ie.otormaigh.playground.persistence.columnadapter
+
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.sqldelight.ColumnAdapter
+import ie.otormaigh.playground.Rover
+
+//FIXME: Add actual parsers here.
+class RoverColumnAdapter(moshi: Moshi) : ColumnAdapter<Rover, String> {
+  private val adapter: JsonAdapter<Rover> = moshi.adapter(Rover::class.java)
+
+  override fun decode(databaseValue: String): Rover = adapter.fromJson(databaseValue) ?: throw Exception("Unable to parse JSON data")
+  override fun encode(value: Rover): String = adapter.toJson(value) ?: ""
+}
