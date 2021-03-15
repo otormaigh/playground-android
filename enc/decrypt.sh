7d19e204de2773df90ba3c6175d1da08a1654881
Elliot Tormey <elliot.tormey@gmail.com>
2021-03-15 23:18:40 +0000

    update enc/encrypt+decrypt script to use actual environment variable

--- enc/decrypt.sh
+++ enc/decrypt.sh
@@ -18,6 +18,7 @@ checkEnv PLAYGROUND_ENCRYPT_KEY
 
 echo "> Decrypting files"
 openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/release.keystore.aes -out signing/release.keystore -k PLAYGROUND_ENCRYPT_KEY
+openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/private_key.pepk.aes -out signing/private_key.pepk -k PLAYGROUND_ENCRYPT_KEY
 openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/enc.properties.aes -out enc.properties -k PLAYGROUND_ENCRYPT_KEY
 echo "> Files decrypted"
 
