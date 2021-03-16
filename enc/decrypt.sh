23b109ee4e98373406412169902fb69e6eece69b
Elliot Tormey <elliot.tormey@gmail.com>
2021-03-16 21:37:20 +0000

    update encrypt/decrypt scripts to replace checkenv function with simple if empty check

--- enc/decrypt.sh
+++ enc/decrypt.sh
@@ -2,24 +2,13 @@
 
 set -e
 
-# Checks if an env value is present and not empty
-checkEnv() {
-  echo "> Checking"
-  env_value=$(printf '%s\n' "${!1}")
-  if [ -z ${env_value} ]; then
-    echo "> $1 is undefined, exiting..."
-    exit 1
-  else
-    echo "> Found value for $1"
-  fi
-}
-
-checkEnv PLAYGROUND_ENCRYPT_KEY
-
-echo "> Decrypting files"
-openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/release.keystore.aes -out signing/release.keystore -k $PLAYGROUND_ENCRYPT_KEY
-openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/private_key.pepk.aes -out signing/private_key.pepk -k $PLAYGROUND_ENCRYPT_KEY
-openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/enc.properties.aes -out enc.properties -k $PLAYGROUND_ENCRYPT_KEY
-echo "> Files decrypted"
-
-echo "> Finishing up"
+if [[ -n "$PLAYGROUND_ENCRYPT_KEY" ]]; then
+  echo "> Decrypting files"
+  openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/release.keystore.aes -out signing/release.keystore -k $PLAYGROUND_ENCRYPT_KEY
+  openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/private_key.pepk.aes -out signing/private_key.pepk -k $PLAYGROUND_ENCRYPT_KEY
+  openssl aes-256-cbc -d -md sha512 -salt -pbkdf2 -iter 10000 -in enc/enc.properties.aes -out enc.properties -k $PLAYGROUND_ENCRYPT_KEY
+  echo "> Files decrypted"
+  echo "> Finishing up"
+else
+  echo "> PLAYGROUND_ENCRYPT_KEY is undefined, exiting..."
+fi
